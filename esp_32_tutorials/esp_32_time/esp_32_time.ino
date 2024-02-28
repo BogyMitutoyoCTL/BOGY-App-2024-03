9b5188d8f89e446400c5372ef1f16ebbf65fec7d
Johannes Zill <Johannes.Zill@MITUTOYO-CTL.DE>
2024-02-28 13:56:32 +0100

    Read unixtime (seconds since 01.01.1970)

--- esp_32_tutorials/esp_32_time/esp_32_time.ino
+++ esp_32_tutorials/esp_32_time/esp_32_time.ino
@@ -5,30 +5,33 @@ RTC_DS3231 rtc;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-void setup () {
-  Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
 
 #ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
+    while (!Serial)
+        ; // wait for serial port to connect. Needed for native USB
 #endif
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    abort();
-  }
+    if (!rtc.begin())
+    {
+        Serial.println("Couldn't find RTC");
+        Serial.flush();
+        abort();
+    }
 
     Serial.println("Setting the time...");
     // When time needs to be set on a new device, or after a power loss, the
     // following line sets the RTC to the date & time this sketch was compiled
-     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+}
 
-
-void loop () {
+void loop()
+{
     DateTime now = rtc.now();
 
     Serial.print(now.year(), DEC);
@@ -46,6 +49,7 @@ void loop () {
     Serial.print(now.second(), DEC);
     Serial.println();
 
+    Serial.print(now.unixtime(), DEC);
     Serial.println();
     delay(3000);
 }
