106ff7322333847cec7c61356ac3ae443e27d0be
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2024-03-19 16:33:49 +0100

    Fix a small issue with languages that differ on startup

--- bluetemp/lib/GlobalState.dart
+++ bluetemp/lib/GlobalState.dart
@@ -5,6 +5,10 @@ class GlobalState {
   static var lightColor = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 7, 66, 234));
   static var darkColor = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 35, 41, 64));
 
+  GlobalState() {
+    languageChanger.set(Sprache);
+  }
+
   var theme = ThemeData(
     colorScheme: lightColor,
     appBarTheme: AppBarTheme(backgroundColor: lightColor.primary, foregroundColor: lightColor.onPrimary),
