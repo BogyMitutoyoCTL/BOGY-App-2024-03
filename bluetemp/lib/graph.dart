630130cc27d71a4af9146838348dcb1b52ce34b6
Simon <simonschlegel07@gmail.com>
2024-03-19 16:22:07 +0100

    Fiexed some bugs...

--- bluetemp/lib/graph.dart
+++ bluetemp/lib/graph.dart
@@ -1,4 +1,5 @@
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class graphs extends StatefulWidget {
@@ -26,11 +27,19 @@ class _graphsState extends State<graphs> {
     return Scaffold(
       appBar: AppBar(title: Text("BlueTemp - Graphen")),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Text(
+            "Temperature graph:",
+            style: TextStyle(fontSize: 35),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text("Temperatur in °C"),
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Text("Temperatur in °C"),
+              ),
             ],
           ),
           Container(
@@ -48,7 +57,10 @@ class _graphsState extends State<graphs> {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text("Zeit in m"),
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                child: Text("Zeit in min"),
+              ),
             ],
           )
         ],
