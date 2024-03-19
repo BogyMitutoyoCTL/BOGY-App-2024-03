67a5e1dcfaca99fe7fd9fb19ec7fdf3a295f5841
Aryan <aryansaibel@gmail.com>
2024-03-19 14:57:55 +0100

    ...

---
+++ bluetemp/lib/Mitprogramierer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Mitprogramierer extends StatefulWidget {
+  const Mitprogramierer({super.key});
+
+  @override
+  State<Mitprogramierer> createState() => _MitprogramiererState();
+}
+
+class _MitprogramiererState extends State<Mitprogramierer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Us")),
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+              height: 450, scale: 0.75, 'assets/Images/Mitutoyo_Logo_1.png'),
+          Text("Name"),
+
+          /// TODO NAME EINTRAGEN
+          Container(
+            height: 40,
+          ),
+          Text("Name"),
+
+          /// TODO NAME EINTRAGEN
+          Container(
+            height: 40,
+          ),
+          Text("Name"),
+
+          /// TODO NAME EINTRAGEN
+          Container(
+            height: 40,
+          ),
+          Text("Aryan Saibel"),
+          Container(
+            height: 40,
+          ),
+          Text("Name")
+
+          /// TODO NAME EINTRAGEN
+        ],
+      ),
+    );
+  }
+}
