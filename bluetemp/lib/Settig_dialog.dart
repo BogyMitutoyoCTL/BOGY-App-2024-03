ee9b3456ea3d8c8a561dd481f2072123ce31f909
Aryan <aryansaibel@gmail.com>
2024-03-19 10:57:03 +0100

    ...

---
+++ bluetemp/lib/Settig_dialog.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class Setting_dialog extends StatefulWidget {
+  const Setting_dialog({super.key});
+
+  @override
+  State<Setting_dialog> createState() => _Setting_dialogState();
+}
+
+class _Setting_dialogState extends State<Setting_dialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Einstellungen"),
+        ),
+      ),
+    );
+  }
+}
