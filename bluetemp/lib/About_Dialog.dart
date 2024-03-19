01740c29b560578bbc22e279bf9d7801114146da
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2024-03-19 11:19:57 +0100

    Add a translation

--- bluetemp/lib/About_Dialog.dart
+++ bluetemp/lib/About_Dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -31,10 +32,9 @@ class _About_DialogState extends State<About_Dialog> {
             Container(
               height: 30,
             ),
-            Image.asset(
-                height: 450, scale: 2, 'assets/Images/BlueTempAppIco.png'),
+            Image.asset(height: 450, scale: 2, 'assets/Images/BlueTempAppIco.png'),
             Text(
-              "BlueTemp",
+              AppLocalizations.of(context).appname,
               style: TextStyle(fontSize: 25),
             ),
             Text(
