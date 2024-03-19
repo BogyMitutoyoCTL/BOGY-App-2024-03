5bd03620cdbf876857d3d6b9193212b955174aaa
Simon <simonschlegel07@gmail.com>
2024-03-19 11:55:40 +0100

    Started with statist

--- bluetemp/lib/statistics.dart
+++ bluetemp/lib/statistics.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class statistic extends StatefulWidget {
   const statistic({super.key});
@@ -14,8 +15,49 @@ class _statisticState extends State<statistic> {
       appBar: AppBar(title: Text("BlueTemp - Statistiken")),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        //TODO: Implement statistic
-        children: [ElevatedButton(onPressed: goBack, child: Text("Zurück"))],
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AppLocalizations.of(context).starttime,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Center(child: Text("16.03.2024 14:00 Uhr")),
+                color: Colors.lightBlue[50],
+                height: 30,
+                width: 150,
+              ),
+            ],
+          ), //TODO: Impelemnt real Start Time
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                AppLocalizations.of(context).endtime,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Center(child: Text("18.03.2024 14:25 Uhr")),
+                color: Colors.lightBlue[50],
+                height: 30,
+                width: 150,
+              ),
+            ],
+          ), //TODO: Impelemnt real End Time
+          ElevatedButton(onPressed: goBack, child: Text("Zurück")),
+        ],
       ),
     );
   }
