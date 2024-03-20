0757ac4693c31ec2e4f570605a6cfbba590fe7ec
Aryan <aryansaibel@gmail.com>
2024-03-20 13:42:40 +0100

    Einheitenauswahl ist nun ein Dropdownbutton anstat von einem radiobutton und vier Einheitenwurden entfernt

--- bluetemp/lib/Setting_Einheiten.dart
+++ bluetemp/lib/Setting_Einheiten.dart
@@ -10,19 +10,6 @@ class Einheiten_Dialog extends StatefulWidget {
 }
 
 class _Einheiten_DialogState extends State<Einheiten_Dialog> {
-  String F = "°F";
-  String C = "°C";
-  String Ra = "°Ra";
-  String Re = "°Ré";
-  String K = "K";
-  String De = "°De";
-  String N = "°N";
-  String O = "°Rø";
-  /* String Hr = "°Hr";
-  String RD = "°RD";
-  String T_RS = "°T_RS";
-  String T_c = "°T_c";
-*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,169 +17,33 @@ class _Einheiten_DialogState extends State<Einheiten_Dialog> {
         title: Center(child: Text("Einheiten Einstellung")),
       ),
       body: Column(children: [
-        RadioListTile(
-          value: C,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungC,
-          title: Text("Celsius"),
-        ),
-        RadioListTile(
-          value: F,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungF,
-          title: Text("Fahrenheit"),
-        ),
-        RadioListTile(
-          value: Ra,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungR,
-          title: Text("Rankine"),
-        ),
-        RadioListTile(
-          value: Re,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungRe,
-          title: Text("Réaumur"),
-        ),
-        RadioListTile(
-          value: K,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungK,
-          title: Text("Kelvin"),
-        ),
-        RadioListTile(
-          value: N,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungN,
-          title: Text("Newton"),
-        ),
-        RadioListTile(
-          value: De,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungDe,
-          title: Text("Delisle"),
-        ),
-        RadioListTile(
-          value: O,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungO,
-          title: Text("Rømer"),
-        ),
-        /*RadioListTile(
-          value: Hr,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungHr,
-          title: Text("Stufe des Hotchkiss-Reaktors"),
-        ),
-        RadioListTile(
-          value: RD,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungRD,
-          title: Text("Temperatureinheit der Röntgen-Dosis"),
-        ),
-        RadioListTile(
-          value: T_RS,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungT_RS,
-          title: Text("Raumzeit-Temperatur"),
-        ),
-        RadioListTile(
-          value: T_c,
-          groupValue: globalState.Einheit,
-          onChanged: Einheit_aenderungT_c,
-          title: Text("Einheit der kosmologischen Temperatur"),
-        )*/
-        // ElevatedButton(
-        //   onPressed: nothing,  //--> Test für Einheiten (// bei void nothing auch entfernen
-        //    child: Text("hhh"),
-        //)
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 8, 20, 0),
+          child: DropdownButton(
+              isExpanded: true,
+              value: globalState.Einheit,
+              items: dropdownItems(),
+              onChanged: Einheits_aenderung),
+        )
       ]),
     );
   }
 
-  void Einheit_aenderungC(String) {
-    setState(() {
-      globalState.Einheit = C;
-      print(globalState.Einheit);
-    });
+  List<DropdownMenuItem<String>> dropdownItems() {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("Celsius"), value: "°C"),
+      DropdownMenuItem(child: Text("Kelvin"), value: "K"),
+      DropdownMenuItem(child: Text("Fahrenheit"), value: "°F"),
+      DropdownMenuItem(child: Text("Newton"), value: "°N"),
+      DropdownMenuItem(child: Text("Rankine"), value: "°Ra"),
+      DropdownMenuItem(child: Text("Réaumur"), value: "°Ré"),
+      DropdownMenuItem(child: Text("Delisle"), value: "°De"),
+      DropdownMenuItem(child: Text("Rømer"), value: "°Rø"),
+    ];
+    return menuItems;
   }
+}
 
-  void Einheit_aenderungF(String) {
-    setState(() {
-      globalState.Einheit = F;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungR(String) {
-    setState(() {
-      globalState.Einheit = Ra;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungRe(String) {
-    setState(() {
-      globalState.Einheit = Re;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungK(String) {
-    setState(() {
-      globalState.Einheit = K;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungN(String) {
-    setState(() {
-      globalState.Einheit = N;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungDe(String) {
-    setState(() {
-      globalState.Einheit = De;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungO(String) {
-    setState(() {
-      globalState.Einheit = O;
-      print(globalState.Einheit);
-    });
-  }
-
-/* void Einheit_aenderungHr(String) {
-    setState(() {
-      globalState.Einheit = Hr;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungRD(String) {
-    setState(() {
-      globalState.Einheit = RD;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungT_RS(String) {
-    setState(() {
-      globalState.Einheit = T_RS;
-      print(globalState.Einheit);
-    });
-  }
-
-  void Einheit_aenderungT_c(String) {
-    setState(() {
-      globalState.Einheit = T_c;
-      print(globalState.Einheit);
-    });
-  }
-
-*/
+void Einheits_aenderung(String? value) {
+  globalState.Einheit = value!;
 }
