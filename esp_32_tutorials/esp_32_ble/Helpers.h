f341ce559739546dda205f2c08e5c48aaeb5fac3
Dieter Blocher <dieter.blocher@mitutoyo-ctl.de>
2024-03-20 14:30:01 +0100

    Add values read / changed output

--- esp_32_tutorials/esp_32_ble/Helpers.h
+++ esp_32_tutorials/esp_32_ble/Helpers.h
@@ -25,6 +25,7 @@ void print_date_time(const DateTime dt, const String prefix = "");
 void print_status_value(const BinaryValue &bv, const String prefix = "");
 void print_buffer_ratio(const CircularBuffer<TemperatureData, BUFFER_SIZE>& temperatures, const String prefix = "");
 void print_buffer_values(const CircularBuffer<TemperatureData, BUFFER_SIZE>& temperatures, const String prefix = "");
+void print_values_read_changed(const long read, const long changed, const String prefix = "");
 std::string get_bt_mac_address();
 std::string get_unique_device_name(const std::string &name);
 bool are_equal(float a, float b);
