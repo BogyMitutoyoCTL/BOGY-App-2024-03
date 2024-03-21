8705b1b2aba5e24224be86fef9d0320594924da6
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2024-03-21 21:47:47 +0100

    Fix #26

--- bluetemp/lib/settings.dart
+++ bluetemp/lib/settings.dart
@@ -14,129 +14,121 @@ class Setting_dialog extends StatefulWidget {
 class _Setting_dialogState extends State<Setting_dialog> {
   @override
   Widget build(BuildContext context) {
+    var verticalSpacing = Container(
+      height: 40,
+    );
+    var horizontalSpacing = Container(
+      width: 14,
+    );
+    var dropdownBackground = globalState.selectedTheme == ThemeMode.dark ? Colors.black : Color.fromARGB(255, 230, 242, 253);
+    var dropdownForeground = TextStyle(
+      color: globalState.selectedTheme == ThemeMode.dark ? Colors.white : Color.fromARGB(255, 0, 0, 0),
+    );
     return Scaffold(
         appBar: AppBar(
           title: Text(AppLocalizations.of(context).appname + " - " + AppLocalizations.of(context).settings_title),
         ),
-        body: SingleChildScrollView(
-            child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-            child: Row(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+              child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            Row(
               children: [
                 Icon(Icons.language),
-                Text("  Spracheinstellungen:"),
+                horizontalSpacing,
+                Text(AppLocalizations.of(context).setting_language),
               ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 8, 20, 0),
-            child: DropdownButton(
+            DropdownButton(
               isExpanded: true,
               value: globalState.Sprache,
-              items: Sprachen_list(),
-              onChanged: Sprachanederung,
-              dropdownColor: globalState.selectedTheme == ThemeMode.dark ? Colors.black : Color.fromARGB(255, 230, 242, 253),
-              style: TextStyle(
-                color: globalState.selectedTheme == ThemeMode.dark ? Colors.white : Color.fromARGB(255, 0, 0, 0),
-              ),
+              items: buildLanguages(),
+              onChanged: onChangeLanguage,
+              dropdownColor: dropdownBackground,
+              style: dropdownForeground,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-            child: Row(
-              children: [Icon(Icons.brush), Text("  Themes:")],
+            verticalSpacing,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Icon(Icons.brush), horizontalSpacing, Text(AppLocalizations.of(context).setting_theme)],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
-            child: DropdownButton(
-                isDense: true,
+            DropdownButton(
                 isExpanded: true,
-                items: Themes_list(),
-                onChanged: Theme_Mode,
-                dropdownColor: globalState.selectedTheme == ThemeMode.dark ? Colors.black : Color.fromARGB(255, 230, 242, 253),
-                style: TextStyle(
-                  color: globalState.selectedTheme == ThemeMode.dark ? Colors.white : Color.fromARGB(255, 0, 0, 0),
-                ),
+                items: buildThemes(),
+                onChanged: onChangeTheme,
+                dropdownColor: dropdownBackground,
+                style: dropdownForeground,
                 value: globalState.selectedTheme),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-            child: Row(
-              children: [Icon(Icons.thermostat), Text(" Temperatureinheit:")],
+            verticalSpacing,
+            Row(
+              children: [Icon(Icons.thermostat), horizontalSpacing, Text(AppLocalizations.of(context).setting_temperature_unit)],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
-            child: DropdownButton(
+            DropdownButton(
               isExpanded: true,
               value: globalState.Einheit,
-              items: dropdownItems(),
-              onChanged: Einheits_aenderung,
-              dropdownColor: globalState.selectedTheme == ThemeMode.dark ? Colors.black : Color.fromARGB(255, 230, 242, 253),
-              style: TextStyle(
-                color: globalState.selectedTheme == ThemeMode.dark ? Colors.white : Color.fromARGB(255, 0, 0, 0),
-              ),
-            ),
-          )
-        ])));
+              items: buildUnits(),
+              onChanged: onChangeUnit,
+              dropdownColor: dropdownBackground,
+              style: dropdownForeground,
+            )
+          ])),
+        ));
   }
-}
 
-List<DropdownMenuItem<String>> dropdownItems() {
-  List<DropdownMenuItem<String>> menuItems = [
-    DropdownMenuItem(child: Text("Celsius"), value: "°C"),
-    DropdownMenuItem(child: Text("Kelvin"), value: "K"),
-    DropdownMenuItem(child: Text("Fahrenheit"), value: "°F"),
-    DropdownMenuItem(child: Text("Newton"), value: "°N"),
-    DropdownMenuItem(child: Text("Rankine"), value: "°Ra"),
-    DropdownMenuItem(child: Text("Réaumur"), value: "°Ré"),
-    DropdownMenuItem(child: Text("Delisle"), value: "°De"),
-    DropdownMenuItem(child: Text("Rømer"), value: "°Rø"),
-  ];
-  return menuItems;
-}
+  List<DropdownMenuItem<String>> buildUnits() {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("Celsius"), value: "°C"),
+      DropdownMenuItem(child: Text("Kelvin"), value: "K"),
+      DropdownMenuItem(child: Text("Fahrenheit"), value: "°F"),
+      DropdownMenuItem(child: Text("Newton"), value: "°N"),
+      DropdownMenuItem(child: Text("Rankine"), value: "°Ra"),
+      DropdownMenuItem(child: Text("Réaumur"), value: "°Ré"),
+      DropdownMenuItem(child: Text("Delisle"), value: "°De"),
+      DropdownMenuItem(child: Text("Rømer"), value: "°Rø"),
+    ];
+    return menuItems;
+  }
 
-void Einheits_aenderung(String? value) {
-  globalState.Einheit = value!;
-  safe.save();
-  globalState.appSettingsChanger.refreshApp();
-}
+  void onChangeUnit(String? value) {
+    globalState.Einheit = value!;
+    safe.save();
+    globalState.appSettingsChanger.refreshApp();
+  }
 
-List<DropdownMenuItem<ThemeMode>> Themes_list() {
-  List<DropdownMenuItem<ThemeMode>> menuItems = [
-    DropdownMenuItem(child: Text("Darkmode"), value: ThemeMode.dark),
-    DropdownMenuItem(
-      child: Text("Lightmode"),
-      value: ThemeMode.light,
-    ),
-    DropdownMenuItem(
-      child: Text("Systemstandard"),
-      value: ThemeMode.system,
-    ),
-  ];
-  return menuItems;
-}
+  List<DropdownMenuItem<ThemeMode>> buildThemes() {
+    List<DropdownMenuItem<ThemeMode>> menuItems = [
+      DropdownMenuItem(child: Text(AppLocalizations.of(context).setting_theme_dark), value: ThemeMode.dark),
+      DropdownMenuItem(
+        child: Text(AppLocalizations.of(context).setting_theme_light),
+        value: ThemeMode.light,
+      ),
+      DropdownMenuItem(
+        child: Text(AppLocalizations.of(context).setting_theme_system),
+        value: ThemeMode.system,
+      ),
+    ];
+    return menuItems;
+  }
 
-void Theme_Mode(ThemeMode? value) {
-  globalState.selectedTheme = value!;
-  globalState.appSettingsChanger.refreshApp();
-}
+  void onChangeTheme(ThemeMode? value) {
+    globalState.selectedTheme = value!;
+    globalState.appSettingsChanger.refreshApp();
+  }
 
-List<DropdownMenuItem<String>> Sprachen_list() {
-  List<DropdownMenuItem<String>> menuItems = [
-    DropdownMenuItem(child: Text("Englisch"), value: "en"),
-    DropdownMenuItem(
-      child: Text("Deutsch"),
-      value: "de",
-    )
-  ];
-  return menuItems;
-}
+  List<DropdownMenuItem<String>> buildLanguages() {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("English"), value: "en"),
+      DropdownMenuItem(
+        child: Text("Deutsch"),
+        value: "de",
+      )
+    ];
+    return menuItems;
+  }
 
-void Sprachanederung(String? value) {
-  globalState.Sprache = value!;
-  safe.save();
-  globalState.appSettingsChanger.setLanguage(value!);
+  void onChangeLanguage(String? value) {
+    globalState.Sprache = value!;
+    safe.save();
+    globalState.appSettingsChanger.setLanguage(value!);
+  }
 }
