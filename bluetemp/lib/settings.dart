2efd17a534987ddf22900d40d6dd64d28f1ce98f
Aryan <aryansaibel@gmail.com>
2024-03-21 16:37:09 +0100

    FIX fehler mit nichterkennbarkeit von Text bei dunklen Theme

--- bluetemp/lib/settings.dart
+++ bluetemp/lib/settings.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:bluetemp/GlobalState.dart';
-import 'SettingsButton.dart';
+
 import 'main.dart';
 
 class Setting_dialog extends StatefulWidget {
